f08bdc4bc3526e489ffc9ad9e698f29dc27e5aec
sagansa <asapanganbangsa@gmail.com>
2026-06-02 22:07:47 +0700

    Update ImageInput.php

--- app/Filament/Forms/ImageInput.php
+++ app/Filament/Forms/ImageInput.php
@@ -14,6 +14,7 @@ class ImageInput extends FileUpload
             ->nullable()
             ->openable()
             ->downloadable()
+            ->fetchFileInformation(false)
             ->disk('public');
     }
 }
